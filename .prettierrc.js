a2d75a4171d16cc7268fe602a4f5e7ae2539a67d
d-rusakov-wp <d.rusakov@webpractik.ru>
2024-09-18 15:20:46 +0300

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    semi: true,
-    singleQuote: true,
-    bracketSpacing: true,
-    bracketSameLine: false,
-    printWidth: 120,
-    arrowParens: "avoid",
-    trailingComma: "es5",
-    plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 120,
+  arrowParens: "avoid",
+  trailingComma: "es5",
+  plugins: ["prettier-plugin-tailwindcss"],
 };
