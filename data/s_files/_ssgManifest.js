57cf1d1c31aba9f53dda6972e1f1fb93aff34b74
rusakov.denis.a <rusakov.denis.a@yandex.ru>
2024-09-22 15:34:49 +0300

    initial

--- data/s_files/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fblog","\u002Fblog\u002F[slug]","\u002Fcareers","\u002Fcareers\u002F[id]","\u002Fcloud-terms-and-conditions","\u002Fcommunity","\u002Finstall","\u002Fpricing","\u002Fprivacy-policy","\u002Fterms-of-use","\u002Fupdates"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
