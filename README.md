d596ad7de264a32e6c83bf54bd6df46f3e2f1698
rusakov.denis.a <rusakov.denis.a@yandex.ru>
2024-09-27 02:34:06 +0300

    initial

--- README.md
+++ README.md
@@ -7,16 +7,21 @@
 ## 🚀 Как начать?
 
 ### Установка зависимостей
+
 ```
 npm ci
 ```
 
 ### Команда для отслеживания изменений
+
 ```
 npm run watch
 ```
 
 ### Команда для prod сборки
+
 ```
 npm run build
 ```
+
+<a href="https://carbon.now.sh/?bg=rgba%2815%2C23%2C42%2C0%29&t=verminal&wt=none&l=text%2Ftypescript-jsx&width=1000&ds=false&dsyoff=20px&dsblur=68px&wc=false&wa=false&pv=7px&ph=6px&ln=false&fl=1&fm=Hack&fs=18px&lh=150%25&si=false&es=1x&wm=false&code=">Carbon</a>
--- custom-scale.js
+++ custom-scale.js
@@ -1,42 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function initScale() {
-        class Scale extends HTMLElement {
-            constructor() {
-                super();
+  function initScale() {
+    class Scale extends HTMLElement {
+      constructor() {
+        super();
+      }
+
+      connectedCallback() {
+        const shadow = this.attachShadow({ mode: 'open' });
+
+        const scale = document.createElement('div');
+        const style = document.createElement('style');
+
+        scale.setAttribute('class', 'scale');
+
+        style.textContent = `
+          .scale {
+            position: relative;
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            height: 40px;
+            background-color: #b3441e;
+            border-radius: 15px 5px 15px 5px;
+            color: #fff;
+            font-weight: bold;
+            margin-bottom: 15px;
+            border: 2px solid #fff;
+
+            &::before {
+              transform: translateX(calc(-100% - 15px));
+              content: '${this.getAttribute('name')}';
+              z-index: 1;
             }
 
-            connectedCallback() {
-                const shadow = this.attachShadow({ mode: 'open' });
-
-                const scale = document.createElement('div');
-                const style = document.createElement('style');
-
-                scale.setAttribute('class', 'scale');
-
-                style.textContent = `
-                  .scale {
-                    width: 100px;
-                    height: 50px;
-                    background-color: red;
-                    border-radius: 5px;
-    
-                    &::after {
-                      content: '${this.getAttribute('name')}';
-                    }
-    
-                    &::before {
-                      content: '${this.getAttribute('percentage')}';
-                    }
-                  }
-                `;
-
-                shadow.appendChild(style);
-                shadow.appendChild(scale);
+            &::after {
+              border-radius: 15px 5px 15px 5px;
+              position: absolute;
+              left: 0;
+              height: 100%;
+              background: repeating-linear-gradient(
+                -45deg,
+                #18404a,
+                #18404a 10px,
+                #fff 10px,
+                #fff 20px
+              );
+              background-color: red;
+              content: '';
+              width: ${this.getAttribute('percentage')}%;
             }
-        }
+          }
+        `;
 
-        customElements.define('custom-scale', Scale);
+        shadow.appendChild(style);
+        shadow.appendChild(scale);
+      }
     }
 
-    initScale();
+    customElements.define('custom-scale', Scale);
+  }
+
+  initScale();
 });
