00e83e703542b05b912800011b8f7aa6a78677b4
rusakov.denis.a <rusakov.denis.a@yandex.ru>
2024-10-24 23:59:06 +0300

    initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,27 @@ module.exports = {
       arsenal: 'Arsenal, sans-serif',
     },
     extend: {
-      fontFamily: { system: [ "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Open Sans", "Helvetica Neue", "sans-serif", "Apple Color Emoji", "Twemoji Mozilla", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji", "EmojiOne Color", "Android Emoji", ],
+      fontFamily: {
+        system: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          'Open Sans',
+          'Helvetica Neue',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Twemoji Mozilla',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+          'EmojiOne Color',
+          'Android Emoji',
+        ],
+      },
       animation: {
         jump: 'jump-left 1.5s linear infinite alternate, jump-top 0.75s cubic-bezier(0, 200, 0.8, 200) infinite',
       },
@@ -31,9 +51,9 @@ module.exports = {
       },
       screens: {
         standalone: {
-        raw: "(display-mode: standalone)"
+          raw: '(display-mode: standalone)',
         },
-      }
+      },
     },
   },
 } satisfies Config;
