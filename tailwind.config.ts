0ec331a0f96b41585cf5d3aadf67f6968bc1e185
d-rusakov-wp <d.rusakov@webpractik.ru>
2024-10-24 13:41:42 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ module.exports = {
       arsenal: 'Arsenal, sans-serif',
     },
     extend: {
+      fontFamily: { system: [ "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Open Sans", "Helvetica Neue", "sans-serif", "Apple Color Emoji", "Twemoji Mozilla", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji", "EmojiOne Color", "Android Emoji", ],
       animation: {
         jump: 'jump-left 1.5s linear infinite alternate, jump-top 0.75s cubic-bezier(0, 200, 0.8, 200) infinite',
       },
