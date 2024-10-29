56ec1b1d7636e8fb81a0197457e1c2082df9b232
d-rusakov-wp <d.rusakov@webpractik.ru>
2024-10-29 18:27:48 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
-    'tailwindcss/nesting': {},
     tailwindcss: {},
+    'tailwindcss/nesting': {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
