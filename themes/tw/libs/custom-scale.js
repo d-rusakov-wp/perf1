dcee9629cd81495f743f348256bc6bf76509bdb4
rusakov.denis.a <rusakov.denis.a@yandex.ru>
2024-10-31 22:50:04 +0300

    initial

--- themes/tw/libs/custom-scale.js
+++ themes/tw/libs/custom-scale.js
@@ -4,10 +4,11 @@ class Scale extends HTMLElement {
   }
 
   renderCSS() {
-    return css`
+    return /* CSS */ `
       .scale {
         position: relative;
         display: flex;
+        align-items: center;
         justify-content: space-between;
         width: 100%;
         height: 40px;
@@ -22,13 +23,14 @@ class Scale extends HTMLElement {
           transform: translateX(calc(-100% - 15px));
           content: '${this.getAttribute('name')}';
           z-index: 1;
+          font: normal 400 24px/1 "Arsenal";
         }
 
         &::after {
           position: absolute;
           left: 0;
           height: 100%;
-          background: repeating-linear-gradient(90deg, #b3441e, #b3441e 20px, #fff 20px, #fff 24px);
+          background: repeating-linear-gradient(90deg, #b3441e, #b3441e 20px, #fff 20px, #fff 23px);
           content: '';
           width: ${this.getAttribute('percentage')}%;
         }
@@ -50,11 +52,9 @@ class Scale extends HTMLElement {
     shadow.appendChild(scale);
   }
 
-  init() {
-    customElements.define('custom-scale', this);
-  }
+  init() {}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  Scale.init();
+  customElements.define('custom-scale', Scale);
 });
