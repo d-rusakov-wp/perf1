c72df7481e222958010ebabd1366fab0105d077d
Русаков Денис <d.rusakov@webpractik.ru>
2024-10-31 16:03:09 +0300

    test

--- themes/tw/libs/custom-scale.js
+++ themes/tw/libs/custom-scale.js
@@ -4,41 +4,36 @@ class Scale extends HTMLElement {
   }
 
   renderCSS() {
-    return `
-          .scale {
-            position: relative;
-            display: flex;
-            justify-content: space-between;
-            width: 100%;
-            height: 40px;
-            background-color: #fff;
-            border-radius: 4px;
-            color: #fff;
-            font-weight: bold;
-            margin-bottom: 15px;
-            border: 4px solid #fff;
+    return css`
+      .scale {
+        position: relative;
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        height: 40px;
+        background-color: #fff;
+        border-radius: 4px;
+        color: #fff;
+        font-weight: bold;
+        margin-bottom: 15px;
+        border: 4px solid #fff;
 
-            &::before {
-              transform: translateX(calc(-100% - 15px));
-              content: '${this.getAttribute('name')}';
-              z-index: 1;
-            }
+        &::before {
+          transform: translateX(calc(-100% - 15px));
+          content: '${this.getAttribute('name')}';
+          z-index: 1;
+        }
 
-            &::after {
-              position: absolute;
-              left: 0;
-              height: 100%;
-              background: repeating-linear-gradient(
-                90deg,
-                #b3441e,
-                #b3441e 20px,
-                #fff 20px,
-                #fff 24px
-              );
-              content: '';
-              width: ${this.getAttribute('percentage')}%;
-            }
-          }`;
+        &::after {
+          position: absolute;
+          left: 0;
+          height: 100%;
+          background: repeating-linear-gradient(90deg, #b3441e, #b3441e 20px, #fff 20px, #fff 24px);
+          content: '';
+          width: ${this.getAttribute('percentage')}%;
+        }
+      }
+    `;
   }
 
   connectedCallback() {
