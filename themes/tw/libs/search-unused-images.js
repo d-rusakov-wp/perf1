eeeab3a6a687ee8de26cada766efce874e184f54
Русаков Денис <d.rusakov@webpractik.ru>
2024-11-05 18:47:35 +0300

    test

---
+++ themes/tw/libs/search-unused-images.js
@@ -0,0 +1,36 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const INDEX_HTML_PATH = './index.html';
+const IMAGES_FOLDER_PATH = './themes/tw';
+const INCLUDED_FORMATS = ['jpg', 'png', 'svg'];
+
+function getExtension(fileName) {
+  return fileName.split('.').pop();
+}
+
+function findMissingImages(folderPath) {
+  const files = fs.readdirSync(folderPath).filter(file => INCLUDED_FORMATS.includes(getExtension(file)));
+  const missingImages = [];
+  const indexHtmlContent = fs.readFileSync(INDEX_HTML_PATH, 'utf8');
+
+  files.forEach(file => {
+    const filePath = path.join(folderPath, file);
+
+    if (fs.lstatSync(filePath).isDirectory()) {
+      missingImages.push(...findMissingImages(filePath));
+
+      return;
+    }
+
+    if (fs.lstatSync(filePath).isFile() && !indexHtmlContent.includes(file)) {
+      missingImages.push(filePath);
+    }
+  });
+
+  return missingImages;
+}
+
+const missingImages = findMissingImages(IMAGES_FOLDER_PATH);
+
+console.log('Неиспользованные изображения:', missingImages);
