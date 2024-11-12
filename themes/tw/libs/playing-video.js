8fee55b049cff4f3888c84c1f56072f20da06178
Русаков Денис <d.rusakov@webpractik.ru>
2024-11-12 16:42:13 +0300

    fix(project): Remove unused code

---
+++ themes/tw/libs/playing-video.js
@@ -0,0 +1,11 @@
+function initPlayingVideo(event) {
+  const element = event.target;
+
+  if (element.tagName !== "VIDEO") return;
+
+  element.paused ? element.play() : element.pause();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", initPlayingVideo);
+});
